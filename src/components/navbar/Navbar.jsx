0edc5067bca5f03543063463505ede706a7a3518
Frans Mazhar <frans.mazhar@mii.co.id>
2022-05-06 19:56:36 +0700

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,21 @@
 import "./navbar.scss"
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import LanguageRoundedIcon from '@mui/icons-material/LanguageRounded';
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." /><SearchRoundedIcon/>
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageRoundedIcon/>English
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
